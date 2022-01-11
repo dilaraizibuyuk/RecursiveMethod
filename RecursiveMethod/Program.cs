a8fe7970f22843c6268ccd6bd689f45b17614723
Dilara <74327986+dilaraizibuyuk@users.noreply.github.com>
2022-01-11 18:53:39 +0300

    Proje dosyası ekle.

---
+++ RecursiveMethod/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecursiveOrnek
+
+{
+    class Program
+    {
+        public static ulong FaktoriyelHesapla(ulong sayi)
+        {
+            if (sayi <= 1)
+            {
+                return 1;
+            }
+            else
+                return sayi * FaktoriyelHesapla(sayi - 1);
+        }
+        public static void Main(string[] args)
+        {
+            
+            for (ulong sayac = 0; sayac <=30; sayac++)
+            {
+                Console.WriteLine("{0}!={1}",sayac,FaktoriyelHesapla(sayac));
+            }
+            Console.ReadLine();
+            
+        }
+    }
+}
